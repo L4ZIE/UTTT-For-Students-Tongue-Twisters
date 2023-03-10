78d343d97584553bd31ab0b82791b953828c213d
Bálint <47783658+Roharx@users.noreply.github.com>
2023-03-11 00:47:45 +0100

    Finished some //TODOs and made the bot able to play

--- src/dk/easv/bll/bot/Scrappy.java
+++ src/dk/easv/bll/bot/Scrappy.java
@@ -14,57 +14,104 @@ public class Scrappy implements IBot {
     private String[][] board;
     private int boardX, boardY;
 
-    private void fillBoard(IGameState state){
+    private void fillBoard(IGameState state) {
         board = state.getField().getBoard();
     }
 
-    private IMove scrappyTactic(IGameState state){
+    //priority moves is a good idea and would make the bot smarter but don't have time to implement
+    //leaving it in for later on when I want to develop the idea further
+    private IMove scrappyTactic(IGameState state, List<IMove> ignoredMoves, List<IMove> prioMoves) {
         fillBoard(state);
         List<IMove> availableMoves = state.getField().getAvailableMoves();
         fillBoardXandY(availableMoves);
-        List<IMove> ignoredMoves = checkRiskyMoves(availableMoves);
         List<IMove> suggestedMoves = new ArrayList<>();
-        Random rnd;
+
+        String player = "1";
+        String ai = "0";
+        if (state.getMoveNumber() % 2 == 0) {
+            player = "0";
+            ai = "1";
+        }
+
 
         //first ignores risky moves:
         for (IMove m : availableMoves) {
-            if (!ignoredMoves.contains(m))
-                suggestedMoves.add(m);
+            if (ignoredMoves != null)
+                if (!ignoredMoves.contains(m))
+                    suggestedMoves.add(m);
         }
 
         //if every move is risky, scraps the ignore list:
-        if(suggestedMoves.isEmpty())
-            for(IMove m : availableMoves){
+        if (suggestedMoves.isEmpty())
+            for (IMove m : availableMoves) {
                 suggestedMoves.add(m);
             }
 
         //if board is empty, place into a random corner that is not ignored:
-        if(availableMoves.size() == 9){
-            rnd = new Random();
-            return calculateCorner(ignoredMoves);
+        if (availableMoves.size() == 9) {
+            return calculateCorner(suggestedMoves);
         }
 
         //if board has a single move and in a corner and middle is not ignored, play middle:
-        if (availableMoves.size() == 8){
-            if (enemyMoveIsInCorner())
+        if (availableMoves.size() == 8) {
+            if (enemyMoveIsInCorner(player))
                 return new Move(1 + boardX, 1 + boardY);
         }
 
+        //if board is about to be completed, complete it (if not ignored)
+        if (getBoardCompletionMove(ai) != null) {
+            return getBoardCompletionMove(ai);
+        }
+
+        //if enemy is about to complete the board, sabotage it
+        if (getBoardCompletionMove(player) != null) {
+            return getBoardCompletionMove(player);
+        }
+
+        return state.getField().getAvailableMoves().get(0);
+    }
+
+    private List<IMove> checkPrioMoves(List<IMove> availableMoves) {
+        //TODO
         return null;
     }
 
-    private boolean enemyMoveIsInCorner() {
+    private IMove getBoardCompletionMove(String currentPlayer) {
         //TODO
-        return false;
+        return null;
     }
 
-    private IMove calculateCorner(List<IMove> ignoredMoves) {
-        //TODO
+    private boolean enemyMoveIsInCorner(String player) {
+        return board[boardX][boardY] == player || board[2 + boardX][boardY] == player ||
+                board[boardX][2 + boardY] == player || board[2 + boardX][2 + boardY] == player;
+    }
+
+    //there's a smarter way but I don't have time to develop it
+    private IMove calculateCorner(List<IMove> suggestedMoves) {
+        for (IMove m : suggestedMoves) {
+            if (m.equals(new Move(boardX, boardY)) || m.equals(new Move(2 + boardX, boardY)) ||
+                    m.equals(new Move(boardX, 2 + boardY)) || m.equals(new Move(2 + boardX, 2 + boardY)))
+                return m;
+        }
         return null;
     }
 
     private void fillBoardXandY(List<IMove> availableMoves) {
-        //TODO
+        if (!availableMoves.isEmpty()) {
+            if (availableMoves.get(0).getX() > 5)
+                boardX = 6;
+            else if (availableMoves.get(0).getX() > 2)
+                boardX = 3;
+            else
+                boardX = 0;
+
+            if (availableMoves.get(0).getY() > 5)
+                boardY = 6;
+            else if (availableMoves.get(0).getY() > 2)
+                boardY = 3;
+            else
+                boardY = 0;
+        }
     }
 
     private List<IMove> checkRiskyMoves(List<IMove> availableMoves) {
@@ -74,11 +121,18 @@ public class Scrappy implements IBot {
     }
 
 
+    private IMove scrappyGlobal(IGameState state) {
+        List<IMove> availableMoves = state.getField().getAvailableMoves();
+        List<IMove> ignoredMoves = checkRiskyMoves(availableMoves);
+        List<IMove> prioMoves = checkPrioMoves(availableMoves);
+
+        return scrappyTactic(state, ignoredMoves, prioMoves);
+    }
 
 
     @Override
     public IMove doMove(IGameState state) {
-        return null;
+        return scrappyGlobal(state);
     }
 
     @Override
